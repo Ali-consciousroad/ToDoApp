fcaf5d9dc092340de82195ff2cff4f270a7b1555
Ali-consciousroad <ali_dindar@live.be>
2021-09-20 12:53:49 +0200

    Setting dynamic inline styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,9 +5,14 @@ import "./CourseInput.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  // We add a new state to know 
   const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    // Remove the error color when we write something again
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -24,7 +29,9 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
+        {/* Dynamically change the text color */}
         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        {/* Dynamically change the border color */}
         <input
           style={{
             borderColor: !isValid ? "red" : "black",
